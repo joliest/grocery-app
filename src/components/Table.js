8c1229403b6a223d43152805c06c330af830f3ab
Joliver Estampador <joliverestampador@gmail.com>
2023-08-15 07:55:45 +0800

    First API call

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,56 +8,59 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-    name,
-    size,
-    store,
-    type,
-    subtype,
-    reference,
-    date,
-) {
-    return { name, size, store, type, subtype, reference, date };
-}
-
-const rows = [
-    createData('Gardenia', 82.00,'SMP Supermarket','Food','Bread','2023-22-02-smp-supermarket','02-23-2023'),
-];
-
-const BasicTable = () => (
-    <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-                <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Size</TableCell>
-                    <TableCell align="right">Store</TableCell>
-                    <TableCell align="right">Type</TableCell>
-                    <TableCell align="right">Subtype</TableCell>
-                    <TableCell align="right">Reference</TableCell>
-                    <TableCell align="right">Date</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {rows.map((row) => (
-                    <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">
-                            {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.size}</TableCell>
-                        <TableCell align="right">{row.store}</TableCell>
-                        <TableCell align="right">{row.type}</TableCell>
-                        <TableCell align="right">{row.subtype}</TableCell>
-                        <TableCell align="right">{row.reference}</TableCell>
-                        <TableCell align="right">{row.date}</TableCell>
+const BasicTable = () => {
+    const [products, setProducts] = useState(null);
+    useEffect(() => {
+        const getProducts = async () => {
+            let fetchedProducts = [];
+            try {
+                fetchedProducts = await axios.get('http://localhost:8080/v1/products')
+            } catch (e) {
+                console.log(e)
+            }
+            return fetchedProducts;
+        }
+        if (!products) {
+            getProducts().then(fetchedProducts => {
+                setProducts(fetchedProducts.data);
+            });
+        }
+    }, [products]);
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Store</TableCell>
+                        <TableCell align="right">Category</TableCell>
+                        <TableCell align="right">Sub Category</TableCell>
+                        <TableCell align="right">Reference</TableCell>
+                        <TableCell align="right">Date Purchased</TableCell>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>
-)
+                </TableHead>
+                <TableBody>
+                    {products && products.map((product) => (
+                        <TableRow
+                            key={product.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {product.name}
+                            </TableCell>
+                            <TableCell align="right">{product.price}</TableCell>
+                            <TableCell align="right">{product.store}</TableCell>
+                            <TableCell align="right">{product.category}</TableCell>
+                            <TableCell align="right">{product.subCategory}</TableCell>
+                            <TableCell align="right">{product.link}</TableCell>
+                            <TableCell align="right">{product.datePurchased}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
 
 export default BasicTable;
